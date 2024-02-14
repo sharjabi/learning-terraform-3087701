e340d088af3215da3ff4c0c09f1122c7bd8dfbd5
sharjabi <159807432+sharjabi@users.noreply.github.com>
2024-02-14 15:43:55 +0300

    Create Security Group

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_block  = ["0.0.0.0/0"]
+  cidr_blocks  = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
 }
