3c5401789621c5337f98a2c11c9160d729471ab8
sharjabi <159807432+sharjabi@users.noreply.github.com>
2024-02-16 01:19:58 +0300

    try again

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "autoscaling" {
   wait_for_capacity_timeout = 0
   health_check_type         = "EC2"
   vpc_zone_identifier       = module.blog_vpc.public_subnets
- // target_group_arns         = modules.blog_alb.target_group_arns
+  //target_group_arns         = modules.blog_alb.target_group_arns
   security_groups           = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
@@ -83,7 +83,7 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-    
+      target_id        = module.autoscaling.autoscaling_group_id
     }
   }
 
