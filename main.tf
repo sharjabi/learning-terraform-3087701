5a41f2cfe1609d519e3c1307eae309172d9daca8
sharjabi <159807432+sharjabi@users.noreply.github.com>
2024-02-16 00:24:03 +0300

    alb

--- main.tf
+++ main.tf
@@ -56,7 +56,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id[0]
+  security_groups = [module.blog_sg.security_group_id]
 
   listeners = {
     ex-http = {
