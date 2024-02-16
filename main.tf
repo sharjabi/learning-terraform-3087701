875d814a22c04d7d238b741bd0093e7dae70b189
sharjabi <159807432+sharjabi@users.noreply.github.com>
2024-02-16 17:33:43 +0300

    try variables

--- main.tf
+++ main.tf
@@ -1,107 +1,41 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["979382823631"] # Bitnami
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t3.nano"
 }
+ 
+variable "ami_filter" {
+  description = "Name filter and owner for AMI"
 
-data "aws_vpc" "default" {
-  default = true
-}
+  type    = object ({
+    name  = string
+    owner = string
+  })
 
-module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "dev"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-
-
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
+variable "environment" {
+  description = "Deployment environment"
 
-
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-
-  # Autoscaling group
-  name = "blog"
-
-  min_size                  = 1
-  max_size                  = 2
-  desired_capacity          = 1
-  wait_for_capacity_timeout = 0
-  health_check_type         = "EC2"
-  vpc_zone_identifier       = module.blog_vpc.public_subnets
-  //target_group_arns         = modules.blog_alb.target_group_arns
-  security_groups           = [module.blog_sg.security_group_id]
-
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-}
-
-
-module "blog_alb" {
-  source = "terraform-aws-modules/alb/aws"
-
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
-  listeners = { 
-    ex-http = {
-      port     = 80
-      protocol = "HTTP"
-     
-      forward = {
-        target_group_key = "ex-instance"
-      }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = module.autoscaling.autoscaling_group_id
-    }
-  }
-
-  tags = {
-    Environment = "dev"
+  type        = object ({
+    name           = string
+    network_prefix = string
+  })
+  default = {
+    name           = "dev"
+    network_prefix = "10.0"
   }
 }
 
-module "blog_sg" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
-  name    = "blog"
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
+  default     = 1
+}
 
-  vpc_id = module.blog_vpc.vpc_id
-
-  ingress_rules = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
+  default     = 2
 }
--- providers.tf
+++ providers.tf
@@ -9,3 +9,14 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+}
+ 
+provider "aws" {
+  region  = "us-west-2"
+}
