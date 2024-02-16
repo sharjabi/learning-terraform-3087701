b693d41a4965dca205f47e45f484c0f54b0e00d8
sharjabi <159807432+sharjabi@users.noreply.github.com>
2024-02-16 17:38:55 +0300

    fix error

--- providers.tf
+++ providers.tf
@@ -9,14 +9,3 @@ terraform {
 provider "aws" {
   region  = "us-west-2"
 }
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
- 
-provider "aws" {
-  region  = "us-west-2"
-}
